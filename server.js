94920105d17afe296bfd68574b929a1152c1ad62
Aleksey <Alexgeorge2863@gmail.com>
2022-04-06 20:06:31 -0600

    add Middleware and import employeeRouter

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ mongoose.connect('mongodb://localhost:27017/employeesDB', () => {
 	console.log(`Connected to the DB successfully`)
 })
 
+app.use('/employees', require('./routes/employeesRouter'))
+
+// error handling middleware
+app.use((err, req, res, next) => {
+	console.log(err)
+	return res.send({ errorMessage: err.message })
+})
+
 app.listen(9000, () => {
 	console.log('The server is running on local Port 9000!')
 })
