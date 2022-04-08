9a3d5aa6929ee0f4651be64efcd005cb61384aef
Aleksey <Alexgeorge2863@gmail.com>
2022-04-08 11:54:23 -0600

    add ability to toggle state between employee card to employee form to update and submit new employees information

--- client/src/components/Employee.js
+++ client/src/components/Employee.js
@@ -12,48 +12,67 @@ export default function Employee({
 	deleteEmployee,
 	updateEmployee,
 }) {
+	const [editToggle, setEditToggle] = useState(false)
+
+	const handleSubmit = (inputs, _id) => {
+		setEditToggle(false)
+		updateEmployee(inputs, _id)
+	}
+
 	return (
 		<div className='employee-card'>
-			<>
-				<h2>
-					Full Name:
-					{firstName} {lastName}
-				</h2>
+			{!editToggle ? (
+				<>
+					<h2>
+						Full Name:
+						{firstName} {lastName}
+					</h2>
+					<img
+						src={image}
+						alt={`add img for ${firstName}`}
+						style={{ height: '200px' }}
+					/>
+					<p>Classification:{classification}</p>
+					<p>Email: {email}</p>
+					<p>Phone number: {phoneNumber}</p>
+					<div className='btn-container'>
+						<button
+							onClick={() => deleteEmployee(_id)}
+							className='btn delete-btn'
+							style={{ margin: '0 1rem' }}
+						>
+							Delete
+						</button>
 
-				<img
-					src={image}
-					alt={`add img for ${firstName}`}
-					style={{ height: '200px' }}
-				/>
-				<p>Classification:{classification}</p>
-				<p>Email: {email}</p>
-				<p>Phone number: {phoneNumber}</p>
-				<div className='btn-container'>
-					{/* !TODO: delete employee btn */}
+						<button
+							className='edit-btn'
+							onClick={() => setEditToggle(prevToggle => !prevToggle)}
+						>
+							Edit
+						</button>
+					</div>
+				</>
+			) : (
+				<>
+					<EmployeeForm
+						firstName={firstName}
+						lastName={lastName}
+						phoneNumber={phoneNumber}
+						classification={classification}
+						email={email}
+						image={image}
+						_id={_id}
+						btnText='Submit Changes'
+						submit={handleSubmit}
+					/>
 					<button
-						onClick={() => deleteEmployee(_id)}
-						className='btn delete-btn'
-						style={{ margin: '0 1rem' }}
+						className='close-btn'
+						onClick={() => setEditToggle(prevToggle => !prevToggle)}
 					>
-						Delete
+						Close
 					</button>
-					{/* !TODO: edit employee btn */}
-					<button className='edit-btn'>Edit</button>
-				</div>
-			</>
-			<>
-				<EmployeeForm
-					firstName={firstName}
-					lastName={lastName}
-					phoneNumber={phoneNumber}
-					classification={classification}
-					email={email}
-					image={image}
-					_id={_id}
-					btnText='Submit Changes'
-				/>
-				<button className='close-btn'>Close</button>
-			</>
+				</>
+			)}
 		</div>
 	)
 }
