6bde29d13158e955f64353e3fa1884f2f6e5a4d5
Aleksey <Alexgeorge2863@gmail.com>
2022-04-13 12:00:42 -0600

    add ability to search employees by name

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
+export default function SearchBar({ placeholder, data }) {
+	const [filterData, setFilterData] = useState([])
+	const [enteredWord, setEnteredWord] = useState('')
 
-export default function SearchBar(props) {
-	const { data, placeholder } = props
+	const handleFilter = event => {
+		const wordEntered = event.target.value
+		setEnteredWord(wordEntered)
+		const newFilter = data.filter(value => {
+			return value.firstName.toLowerCase().includes(wordEntered.toLowerCase())
+		})
+		if (wordEntered === '') {
+			setFilterData([])
+		} else {
+			setFilterData(newFilter)
+		}
+	}
+
+	const clearInput = () => {
+		setFilterData([])
+		setEnteredWord('')
+	}
 
 	return (
 		<div className='search'>
 			<div className='searchInputs'>
-				<input type='text' placeholder={placeholder} />
-				<div className='searchIcon'></div>
+				<input
+					type='text'
+					value={enteredWord}
+					placeholder={placeholder}
+					onChange={handleFilter}
+				/>
+				<div className='searchIcon'>
+					{filterData.length === 0 ? (
+						<SearchIcon />
+					) : (
+						<CloseIcon id='clearBtn' onClick={clearInput} />
+					)}
+				</div>
 			</div>
-			<div className='dataResult'></div>
+			{filterData.length !== 0 && (
+				<div className='dataResult'>
+					{filterData.slice(0, 1).map((value, key) => (
+						<a
+							key={key}
+							className='dataItem'
+							href={value.image}
+							target='_black'
+						>
+							<p>
+								{value.firstName} {value.lastName}
+							</p>
+						</a>
+					))}
+				</div>
+			)}
 		</div>
 	)
 }
